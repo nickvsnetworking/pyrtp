825f7864117f8c1160e1fa6d43b5611f55df9700
Nick <nick@nickvsnetworking.com>
2019-07-08 13:55:35 +1000

    Basic decoder added

--- pyrtp.py
+++ pyrtp.py
@@ -2,10 +2,10 @@
 
 def GenerateRTPpacket(rtp_params):
 
-    #Example Usage:
-    ##payload = 'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
-    ##rtp_params = {'version' : 2, 'padding' : 0, 'extension' : 0, 'csi_count' : 0, 'marker' : 0, 'payload_type' : 8, 'sequence_number' : 306, 'timestamp' : 306, 'ssrc' : 185755418, 'payload' : payload}
-    ##GenerateRTPpacket(rtp_params)             #Generates hex to send down the wire
+    ##Example Usage:
+    #payload = 'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+    #rtp_params = {'version' : 2, 'padding' : 0, 'extension' : 0, 'csi_count' : 0, 'marker' : 0, 'payload_type' : 8, 'sequence_number' : 306, 'timestamp' : 306, 'ssrc' : 185755418, 'payload' : payload}
+    #GenerateRTPpacket(rtp_params)             #Generates hex to send down the wire
 
 
     
@@ -25,7 +25,6 @@ def GenerateRTPpacket(rtp_params):
     payload_type = str(format(rtp_params['payload_type'], 'b').zfill(7))        #7 bit Payload Type (From https://tools.ietf.org/html/rfc3551#section-6)
     byte2 = format(int((marker + payload_type), 2), 'x').zfill(2)                           #Convert binary values to an int then format that as hex with 2 bytes of padding if required
 
-
     
     sequence_number = format(rtp_params['sequence_number'], 'x').zfill(4)                               #16 bit sequence number (Starts from a random position and incriments per packet)
     
@@ -44,3 +43,30 @@ def GenerateRTPpacket(rtp_params):
 
     
 
+def DecodeRTPpacket(packet_bytes):
+    packet_vars = {}
+    byte1 = packet_bytes[0:2]           #Byte1 as Hex
+    byte1 = int(byte1, 16)              #Convert to Int
+    byte1 = format(byte1, 'b')          #Convert to Binary
+    packet_vars['version'] = int(byte1[0:2], 2)     #Get RTP Version
+    packet_vars['padding'] = int(byte1[2:3])        #Get padding bit
+    packet_vars['extension'] = int(byte1[3:4])        #Get extension bit
+    packet_vars['csi_count'] = int(byte1[4:8], 2)     #Get RTP Version
+
+    byte2 = packet_bytes[2:4]
+
+    byte2 = int(byte2, 16)              #Convert to Int
+    byte2 = format(byte2, 'b').zfill(8) #Convert to Binary
+    packet_vars['marker'] = int(byte2[0:1])
+    packet_vars['marker'] = int(byte2[1:8], 2)
+
+    packet_vars['sequence_number'] = int(str(packet_bytes[4:8]), 16)
+
+    packet_vars['timestamp'] = int(str(packet_bytes[8:16]), 16)
+
+    packet_vars['ssrc'] = int(str(packet_bytes[16:24]), 16)
+
+    packet_vars['payload'] = str(packet_bytes[24:])
+    print(packet_vars)
+packet_bytes = '8008d4340000303c0b12671ad5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+DecodeRTPpacket(packet_bytes)
